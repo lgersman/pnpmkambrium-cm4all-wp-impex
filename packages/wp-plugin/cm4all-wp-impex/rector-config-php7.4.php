4663f89fc8a95a92ca62315335298b366805a1ce
Lars Gersmann <lars.gersmann@gmail.com>
2023-10-23 20:18:11 +0200

    fix(@pnpmkambrium-cm4all-wp-impex/cm4all-wp-impex): fixed rector config to work with latest rector

--- packages/wp-plugin/cm4all-wp-impex/rector-config-php7.4.php
+++ packages/wp-plugin/cm4all-wp-impex/rector-config-php7.4.php
@@ -9,16 +9,26 @@ use Rector\Set\ValueObject\DowngradeSetList;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-  // get parameters
-  $parameters = $rectorConfig->parameters();
-  $parameters->set(Option::PATHS, [
-    __DIR__ . '/dist/cm4all-wp-impex-php7.4.0'
-  ]);
-  $parameters->set(Option::PARALLEL, false);
+  // // get parameters
+  // $parameters = $rectorConfig->parameters();
+  // $parameters->set(Option::PATHS, [
+  //   __DIR__ . '/dist/cm4all-wp-impex-php7.4.0'
+  // ]);
+  // $rectorConfig->paths([__DIR__ . '/dist/cm4all-wp-impex-php7.4.0']); #
 
-  $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
+  // $parameters->set(Option::PARALLEL, false);
+  $rectorConfig->disableParallel();
+
+  // $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
+  $rectorConfig->phpVersion(PhpVersion::PHP_80);
 
   // Define what rule sets will be applied
-  $rectorConfig->import(DowngradeLevelSetList::DOWN_TO_PHP_80);
-  $rectorConfig->import(DowngradeSetList::PHP_80);
+  // $rectorConfig->import(DowngradeLevelSetList::DOWN_TO_PHP_80);
+  // $rectorConfig->import(DowngradeSetList::PHP_80);
+  $rectorConfig->sets(
+    [
+      DowngradeLevelSetList::DOWN_TO_PHP_80,
+      DowngradeSetList::PHP_80
+    ]
+  );
 };
