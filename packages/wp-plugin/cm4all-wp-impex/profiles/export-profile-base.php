001173fcb431f8c1b8bce431d0f02481eb211024
Lars Gersmann <lars.gersmann@cm4all.com>
2023-11-20 13:10:29 +0100

    refactor(@pnpmkambrium-cm4all-wp-impex/cm4all-wp-impex): export wp_attachment_pages_enabled option

--- packages/wp-plugin/cm4all-wp-impex/profiles/export-profile-base.php
+++ packages/wp-plugin/cm4all-wp-impex/profiles/export-profile-base.php
@@ -22,16 +22,17 @@ $profile->addTask('wordpress attachments (uploads)', AttachmentsExporter::PROVID
 
 // export most common used wp options
 $profile->addTask(
-  'common wp_options', 
-  WpOptionsExporter::PROVIDER_NAME, 
+  'common wp_options',
+  WpOptionsExporter::PROVIDER_NAME,
   [WpOptionsExporter::OPTION_SELECTOR => [
     'page_on_front', // wordpress homepage
     'show_on_front', // what should be shown ('page'| 'post' ...)
     'page_for_posts', // posts homepage
 
     'blogname',       // aka website title
-    'blogdescription', 
+    'blogdescription',
     'site_logo',       // website logo
-    'site_icon'       // website icon
+    'site_icon',       // website icon
+    'wp_attachment_pages_enabled', // enable/disable attachment pages (see https://make.wordpress.org/core/2023/10/16/changes-to-attachment-pages/)
   ],],
 );
